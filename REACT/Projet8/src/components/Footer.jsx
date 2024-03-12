7e446da6adca0922779fd555a6267a8bf3792a6f
Imi59 <imane.baqua@hotmail.fr>
2024-03-12 17:38:06 +0100

    composant footer ok

--- REACT/Projet8/src/components/Footer.jsx
+++ REACT/Projet8/src/components/Footer.jsx
@@ -1,7 +1,10 @@
-function Footer () {
+import LOGO from "../assets/images/LOGO.png";
+
+function Footer() {
   return (
     <div className="footer">
-      
+      <img src={LOGO} alt="LOGO KASA BLANC" />
+      <p> © 2020 Kasa. All rights reserved </p>
     </div>
   );
 }
