01861f2d8d029ec92ac2d3103990457b70bcafff
Imi59 <imane.baqua@hotmail.fr>
2024-03-16 11:18:04 +0100

    finalisation de la page détail des logements

--- REACT/Projet8/src/components/Logement.jsx
+++ REACT/Projet8/src/components/Logement.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react"; // Importez useState depuis React
 import { useParams } from "react-router-dom";
 import logements from "../data/logements.json";
 import Carousel from "./carousel.jsx"; // Importez le composant Carousel
 import emptyStar from "../assets/images/empty-star.png"; // Importez l'image de l'étoile vide
 import filledStar from "../assets/images/filled-star.png"; // Importez l'image de l'étoile remplie
+import arrowDown from "../assets/images/arrow-down.png"; // Importez l'image de la flèche vers le bas
+import arrowUp from "../assets/images/arrow-up.png"; // Importez l'image de la flèche vers le haut
 
 const findLogementID = (id) => {
   return logements.find((logement) => logement.id === id);
@@ -12,6 +14,21 @@ const findLogementID = (id) => {
 const Logement = () => {
   const { id } = useParams();
   const logement = findLogementID(id);
+
+  // Déclaration de l'état local pour suivre si la description est visible ou cachée
+  const [descriptionVisible, setDescriptionVisible] = useState(true);
+  // Déclaration de l'état local pour suivre si les équipements sont visibles ou cachés
+  const [equipmentsVisible, setEquipmentsVisible] = useState(true);
+
+  // Fonction pour basculer la visibilité de la description
+  const toggleDescriptionVisibility = () => {
+    setDescriptionVisible(!descriptionVisible);
+  };
+
+  // Fonction pour basculer la visibilité des équipements
+  const toggleEquipmentsVisibility = () => {
+    setEquipmentsVisible(!equipmentsVisible);
+  };
 
   // Séparer le nom complet de l'hôte en nom et prénom
   const hostNameParts = logement.host.name.split(" ");
@@ -42,32 +59,66 @@ const Logement = () => {
   return (
     <div>
       <Carousel images={logement.pictures} />
-      <div>
+      <div className="part1">
         <div className="title">
           <h1>{logement.title}</h1>
           <p>{logement.location}</p>
+          <ul className="tags">
+            {logement.tags.map((tag, index) => (
+              <li key={index}>{tag}</li>
+            ))}
+          </ul>
         </div>
-        <div className="host">
-          <p>
-            {hostFirstName} <br /> {hostLastName}
-          </p>
-          <img src={logement.host.picture} alt={logement.host.name} />
+        <div className="host-rating">
+          <div className="host">
+            <p>
+              {hostFirstName} <br /> {hostLastName}
+            </p>
+            <img src={logement.host.picture} alt={logement.host.name} />
+          </div>
+          <p className="stars">{renderRatingStars(logement.rating)}</p>
         </div>
       </div>
-      <p>{logement.description}</p>
-      <p>{renderRatingStars(logement.rating)}</p>
-      <h2>Equipments:</h2>
-      <ul>
-        {logement.equipments.map((equipment, index) => (
-          <li key={index}>{equipment}</li>
-        ))}
-      </ul>
-      <h2>Tags:</h2>
-      <ul>
-        {logement.tags.map((tag, index) => (
-          <li key={index}>{tag}</li>
-        ))}
-      </ul>
+      <div className="collapse-section">
+        <div className="collapse1">
+          <div className="description">
+            <h2>Description </h2>
+            {/* Bouton pour basculer la visibilité de la description */}
+            <div className="btn-collapse" onClick={toggleDescriptionVisibility}>
+              {/* Affichage de la flèche vers le haut ou vers le bas en fonction de l'état de la visibilité */}
+              <img
+                src={descriptionVisible ? arrowUp : arrowDown}
+                alt="Toggle description"
+              />
+            </div>
+          </div>
+
+          {/* Affichage de la description uniquement si elle est visible */}
+          {descriptionVisible && <p>{logement.description}</p>}
+        </div>
+        <div className="collapse2">
+          {/* Section pour les équipements */}
+          <div className="equipements">
+            <h2>Equipments</h2>
+            {/* Bouton pour basculer la visibilité des équipements */}
+            <div className="btn-collapse" onClick={toggleEquipmentsVisibility}>
+              {/* Affichage de la flèche vers le haut ou vers le bas en fonction de l'état de la visibilité */}
+              <img
+                src={equipmentsVisible ? arrowUp : arrowDown}
+                alt="Toggle equipments"
+              />
+            </div>
+          </div>
+          {/* Affichage des équipements uniquement s'ils sont visibles */}
+          {equipmentsVisible && (
+            <ul>
+              {logement.equipments.map((equipment, index) => (
+                <li key={index}>{equipment}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
