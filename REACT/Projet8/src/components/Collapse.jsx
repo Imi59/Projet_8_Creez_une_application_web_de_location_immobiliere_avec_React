7ba6dce858ca3cd136b0e7f624f0e248b53af7b1
Imi59 <imane.baqua@hotmail.fr>
2024-03-16 13:20:13 +0100

    création composant collapse

---
+++ REACT/Projet8/src/components/Collapse.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import arrowUp from "../assets/images/arrow-up.png"; // Importez l'image de la flèche vers le haut
+import arrowDown from "../assets/images/arrow-down.png"; // Importez l'image de la flèche vers le bas
+
+const Collapse = ({ title, defaultOpen, isOpen, onToggle, children }) => {
+  // Fonction pour basculer l'état du collapse
+  const toggleCollapse = () => {
+    onToggle(!isOpen);
+  };
+
+  return (
+    // Conteneur principal du collapse avec une classe conditionnelle "open" si le collapse est ouvert
+    <div className={`collapse-section ${isOpen ? "open" : ""}`}>
+      {/* En-tête du collapse, cliquable pour basculer l'état */}
+      <div className="collapse-header" onClick={toggleCollapse}>
+        {/* Titre du collapse */}
+        <h2>{title}</h2>
+        {/* Affichage de la flèche vers le haut ou vers le bas en fonction de l'état */}
+        <img src={isOpen ? arrowUp : arrowDown} alt={`Toggle ${title}`} />
+      </div>
+      {/* Contenu du collapse, affiché uniquement s'il est ouvert */}
+      {isOpen && <div className="collapse-content">{children}</div>}
+    </div>
+  );
+};
+
+export default Collapse;
--- REACT/Projet8/src/components/Logement.jsx
+++ REACT/Projet8/src/components/Logement.jsx
@@ -1,19 +1,46 @@
-import React, { useState } from "react"; // Importez useState depuis React
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import logements from "../data/logements.json";
+import logements from "../data/logements.json"; // Importez les données des logements
 import Carousel from "./carousel.jsx"; // Importez le composant Carousel
-import emptyStar from "../assets/images/empty-star.png"; // Importez l'image de l'étoile vide
-import filledStar from "../assets/images/filled-star.png"; // Importez l'image de l'étoile remplie
-import arrowDown from "../assets/images/arrow-down.png"; // Importez l'image de la flèche vers le bas
-import arrowUp from "../assets/images/arrow-up.png"; // Importez l'image de la flèche vers le haut
+import filledStar from "../assets/images/filled-star.png"; // Importez l'image d'étoile remplie
+import emptyStar from "../assets/images/empty-star.png"; // Importez l'image d'étoile vide
+import Collapse from "./Collapse"; // Importez le nouveau composant Collapse
 
+// Fonction pour trouver le logement en fonction de son ID
 const findLogementID = (id) => {
   return logements.find((logement) => logement.id === id);
 };
 
 const Logement = () => {
-  const { id } = useParams();
-  const logement = findLogementID(id);
+  const { id } = useParams(); // Obtenez l'ID du logement à partir des paramètres de l'URL
+  const logement = findLogementID(id); // Recherchez le logement correspondant à l'ID
+
+  // Séparer le nom complet de l'hôte en prénom et nom de famille
+  const hostNameParts = logement.host.name.split(" ");
+  const hostFirstName = hostNameParts[0];
+  const hostLastName = hostNameParts.slice(1).join(" ");
+
+  // Fonction pour générer les étoiles de notation en fonction du rating
+  const renderRatingStars = (rating) => {
+    const filledStarsCount = Math.round(rating);
+    const emptyStarsCount = 5 - filledStarsCount;
+
+    const stars = [];
+
+    // Ajout des étoiles pleines
+    for (let i = 0; i < filledStarsCount; i++) {
+      stars.push(<img key={i} src={filledStar} alt="Filled star" />);
+    }
+
+    // Ajout des étoiles vides
+    for (let i = 0; i < emptyStarsCount; i++) {
+      stars.push(
+        <img key={filledStarsCount + i} src={emptyStar} alt="Empty star" />
+      );
+    }
+
+    return stars;
+  };
 
   // Déclaration de l'état local pour suivre si la description est visible ou cachée
   const [descriptionVisible, setDescriptionVisible] = useState(true);
@@ -30,39 +57,16 @@ const Logement = () => {
     setEquipmentsVisible(!equipmentsVisible);
   };
 
-  // Séparer le nom complet de l'hôte en nom et prénom
-  const hostNameParts = logement.host.name.split(" ");
-  const hostFirstName = hostNameParts[0];
-  const hostLastName = hostNameParts.slice(1).join(" "); // Le reste après le premier mot est considéré comme le nom de famille
-
-  // Fonction pour générer les étoiles en fonction du rating
-  const renderRatingStars = (rating) => {
-    const filledStarsCount = Math.round(rating); // Nombre d'étoiles pleines
-    const emptyStarsCount = 5 - filledStarsCount; // Nombre d'étoiles vides
-
-    const stars = [];
-
-    // Ajouter les étoiles pleines
-    for (let i = 0; i < filledStarsCount; i++) {
-      stars.push(<img key={i} src={filledStar} alt="Filled star" />);
-    }
-    // Ajouter les étoiles vides
-    for (let i = 0; i < emptyStarsCount; i++) {
-      stars.push(
-        <img key={filledStarsCount + i} src={emptyStar} alt="Empty star" />
-      );
-    }
-
-    return stars;
-  };
-
   return (
     <div>
+      {/* Carousel pour afficher les images du logement */}
       <Carousel images={logement.pictures} />
       <div className="part1">
         <div className="title">
+          {/* Titre et emplacement du logement */}
           <h1>{logement.title}</h1>
           <p>{logement.location}</p>
+          {/* Tags associés au logement */}
           <ul className="tags">
             {logement.tags.map((tag, index) => (
               <li key={index}>{tag}</li>
@@ -72,52 +76,37 @@ const Logement = () => {
         <div className="host-rating">
           <div className="host">
             <p>
-              {hostFirstName} <br /> {hostLastName}
+              {hostFirstName} <p>{hostLastName}</p>
             </p>
             <img src={logement.host.picture} alt={logement.host.name} />
           </div>
+          {/* Affichage de la notation de l'hôte */}
           <p className="stars">{renderRatingStars(logement.rating)}</p>
         </div>
       </div>
-      <div className="collapse-section">
-        <div className="collapse1">
-          <div className="description">
-            <h2>Description </h2>
-            {/* Bouton pour basculer la visibilité de la description */}
-            <div className="btn-collapse" onClick={toggleDescriptionVisibility}>
-              {/* Affichage de la flèche vers le haut ou vers le bas en fonction de l'état de la visibilité */}
-              <img
-                src={descriptionVisible ? arrowUp : arrowDown}
-                alt="Toggle description"
-              />
-            </div>
-          </div>
-
-          {/* Affichage de la description uniquement si elle est visible */}
-          {descriptionVisible && <p>{logement.description}</p>}
-        </div>
-        <div className="collapse2">
-          {/* Section pour les équipements */}
-          <div className="equipements">
-            <h2>Equipments</h2>
-            {/* Bouton pour basculer la visibilité des équipements */}
-            <div className="btn-collapse" onClick={toggleEquipmentsVisibility}>
-              {/* Affichage de la flèche vers le haut ou vers le bas en fonction de l'état de la visibilité */}
-              <img
-                src={equipmentsVisible ? arrowUp : arrowDown}
-                alt="Toggle equipments"
-              />
-            </div>
-          </div>
-          {/* Affichage des équipements uniquement s'ils sont visibles */}
-          {equipmentsVisible && (
-            <ul>
-              {logement.equipments.map((equipment, index) => (
-                <li key={index}>{equipment}</li>
-              ))}
-            </ul>
-          )}
-        </div>
+      <div className="collapse-part">
+        {/* Utilisation du composant Collapse pour afficher la description */}
+        <Collapse
+          title="Description"
+          defaultOpen={true}
+          isOpen={descriptionVisible}
+          onToggle={toggleDescriptionVisibility}
+        >
+          <p>{logement.description}</p>
+        </Collapse>
+        {/* Utilisation du composant Collapse pour afficher les équipements */}
+        <Collapse
+          title="Equipments"
+          defaultOpen={true}
+          isOpen={equipmentsVisible}
+          onToggle={toggleEquipmentsVisibility}
+        >
+          <ul>
+            {logement.equipments.map((equipment, index) => (
+              <li key={index}>{equipment}</li>
+            ))}
+          </ul>
+        </Collapse>
       </div>
     </div>
   );
