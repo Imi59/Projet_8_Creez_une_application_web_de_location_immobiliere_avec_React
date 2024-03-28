47d22c96466b6cab2731e3705075e6419b3322ad
Imi59 <imane.baqua@hotmail.fr>
2024-03-28 20:21:14 +0100

    finalisation style SASS

--- REACT/Projet8/src/components/Banner2.jsx
+++ REACT/Projet8/src/components/Banner2.jsx
@@ -5,7 +5,6 @@ function Banner2() {
     <div>
       <div className="banner2">
         <img src={ImgBanner1} alt="source" />
-        <h1>Chez vous, partout et ailleurs</h1>
       </div>
     </div>
   );
