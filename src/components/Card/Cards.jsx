57aed3dba870e2c02eea79af38c5b225b8727508
Imi59 <imane.baqua@hotmail.fr>
2024-03-29 17:32:06 +0100

    nettoyage du code avant soutenance

--- src/components/Card/Cards.jsx
+++ src/components/Card/Cards.jsx
@@ -1,8 +1,6 @@
-//Dans React, les composants peuvent recevoir des données, appelées "props" (propriétés),
-// de la part de leurs composants parents. Ces props sont passées
-// en tant qu'arguments aux composants lors de leur utilisation
+
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom"; // pour faire le lien avec ma card et son explication 
+import { Link } from "react-router-dom";
 
 function Cards({ Logements }) {
   return(
