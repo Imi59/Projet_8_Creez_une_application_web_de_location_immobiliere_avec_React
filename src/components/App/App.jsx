aae18d653e640057032969ff61eb0a8dea82e1e9
Imi59 <imane.baqua@hotmail.fr>
2024-03-29 14:16:16 +0100

    suppression commentaires inutiles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,20 @@
-// j'importe mes 3 composants pour pouvoir les appeler dans mes routes
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import About from "../../pages/About";
 import Home from "../../pages/Home";
 import Logement from "../Logement/Logement";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ErrorPage from "../ErrorPage/ErrorPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./app.scss";
 
 function App() {
   return (
-    //Schema type pour gérer des navigation d'une page à une autre
-    // le header une seule fois au dessus des routes permet de se répéter sur chaque page
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="logement/:id" element={<Logement />} />
-        {/* Route pour gérer les routes non trouvées */}
         <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,15 @@
 import Banner from "../components/Banner1/Banner";
 import Cards from "../components/Card/Cards";
-// je dois importer mon dossier json avec les logements pour pouvoir les afficher dynamiquement
-import Logements from "../data/logements.json";
+import Appts from "../data/logements.json";
 
 function Home() {
   return (
     <div className="home">
       <Banner />
       <div className="gallery">
-        <Cards Logements={Logements} />
+        <Cards Logements={Appts} />
       </div>
     </div>
   );
 }
 export default Home;
-
-//installation react rooter dom // npm i react-router-dom
