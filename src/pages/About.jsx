b3da53bd6df95d693ce849b041a7a10a5e33cec9
Imi59 <imane.baqua@hotmail.fr>
2024-03-29 14:24:53 +0100

    suppression commentaires inutiles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from "react"; // Importer useState si nécessaire
+import React, { useState } from "react"; 
 import Collapse from "../components/Collapse/Collapse";
 import Banner2 from "../components/Banner2/Banner2";
 import AboutDatas from "../data/aboutArray.json";
 
 function About() {
-  // Définir l'état local pour suivre l'état de chaque Collapse
+
   const [collapseStates, setCollapseStates] = useState(
-    Array(AboutDatas.length).fill(false) // Initialiser tous les Collapse ouverts
+    Array(AboutDatas.length).fill(false)
   );
 
-  // Fonction pour basculer l'état d'un Collapse spécifique à un index donné
   const handleToggle = (index) => {
     const newCollapseStates = [...collapseStates];
     newCollapseStates[index] = !newCollapseStates[index];
